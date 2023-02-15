e925a02281a624a23a88e43cf205151b9eceebce
yoga980393 <wggisdde@gmail.com>
2023-02-16 00:23:40 +0800

    加入專案檔案。

---
+++ HomeWork_20230215_01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_20230215_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
